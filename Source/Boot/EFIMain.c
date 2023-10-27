11276955eee36d17ee5c64f594e5f15ecc1adfa4
Kian738 <56565697+Kian738@users.noreply.github.com>
2023-10-27 07:31:53 +0200

    Print hello world

--- Source/Boot/EFIMain.c
+++ Source/Boot/EFIMain.c
@@ -1,10 +1,6 @@
 #include "UEFI.h"
 
-static void* uefi_image_handle;
-static EFI_SYSTEM_TABLE uefi_system_table;
-
-EFI_STATUS efi_main(void* image_handle, EFI_SYSTEM_TABLE system_table)
+EFI_STATUS EfiMain(EFI_HANDLE imageHandle, EFI_SYSTEM_TABLE *systemTable)
 {
-    uefi_image_handle = image_handle;
-    uefi_system_table = system_table;
+    systemTable->ConOut->OutputString(systemTable->ConOut, L"Hello, World!\n\r");
 }
