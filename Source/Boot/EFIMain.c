75e671626e746417270fd1b2c138f9bbd3e74cb1
Kian738 <56565697+Kian738@users.noreply.github.com>
2023-10-27 08:16:38 +0200

    Add cmake

--- Source/Boot/EFIMain.c
+++ Source/Boot/EFIMain.c
@@ -3,4 +3,6 @@
 EFI_STATUS EfiMain(EFI_HANDLE imageHandle, EFI_SYSTEM_TABLE *systemTable)
 {
     systemTable->ConOut->OutputString(systemTable->ConOut, L"Hello, World!\n\r");
+    
+    return EFI_SUCCESS;
 }
