9f57cfecea682247efc071f57bd398b55e1ef824
Kian738 <56565697+Kian738@users.noreply.github.com>
2023-10-28 18:39:36 +0200

    Add memory page stuff

--- Source/Boot/Main.c
+++ Source/Boot/Main.c
@@ -15,5 +15,28 @@ EFI_STATUS EFIMain(EFI_HANDLE imageHandle, EFI_SYSTEM_TABLE *systemTable)
     PrintLn(L"Shutting down...");
     Shutdown();
 
+    EFI_PHYSICAL_ADDRESS physicalBuffer;
+    UINT64 allocSize = (1 + 64) * 1024 * 1024;
+    UINT64 pages = allocSize / 4096;
+    EFI_CALL(SystemTable->BootServices->AllocatePages(
+        AllocateAnyPages,
+        EfiBootServicesData,
+        pages,
+        &physicalBuffer));
+
+    EFI_PHYSICAL_ADDRESS physicalBufferKernelFile;
+    UINT64 maxKernelFileSize = 4 * 1024 * 1024;
+    UINT64 kernelFilePages = maxKernelFileSize / 4096;
+    EFI_CALL(SystemTable->BootServices->AllocatePages(
+        AllocateAnyPages,
+        EfiBootServicesData,
+        kernelFilePages,
+        &physicalBufferKernelFile));
+
+    UINT64 stackStart = physicalBuffer;
+    UINT64 stackSize = 1024 * 1024;
+    UINT64 kernelStart = stackStart + stackSize;
+    UINT64 kernelMaxSize = allocSize - stackSize;
+
     return EFI_SUCCESS;
 }
