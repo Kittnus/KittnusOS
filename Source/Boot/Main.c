f8519a300f9f6b214de3f236631f82d1c98bfeb2
Kian738 <56565697+Kian738@users.noreply.github.com>
2023-10-28 07:55:26 +0200

    Add workflow

--- Source/Boot/Main.c
+++ Source/Boot/Main.c
@@ -3,7 +3,7 @@
 EFI_STATUS EFIMain(EFI_HANDLE imageHandle, EFI_SYSTEM_TABLE *systemTable)
 {
     InitializeEFI(imageHandle, systemTable);
-    
+
     PrintLn(L"Hello, world!");
 
     SetColor(EFI_LIGHTGREEN);
