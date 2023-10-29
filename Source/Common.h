15e696d40713253f59b2fbe9d3c9c788c2168129
Jacob <76225335+Jacobb626@users.noreply.github.com>
2023-10-29 15:41:22 -0400

    fix types

--- Source/Common.h
+++ Source/Common.h
@@ -1,10 +1,5 @@
 #pragma once
 
-#define TRUE 1
-#define FALSE 0
-
-typedef UINT8 BOOLEAN;
-
 typedef signed char INT8;
 typedef signed short INT16;
 typedef signed int INT32;
@@ -14,3 +9,8 @@ typedef unsigned char UINT8;
 typedef unsigned short UINT16;
 typedef unsigned int UINT32;
 typedef unsigned long long UINT64;
+
+#define TRUE 1
+#define FALSE 0
+
+typedef UINT8 BOOLEAN;
