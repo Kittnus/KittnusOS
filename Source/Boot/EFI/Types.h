2a5dab1452f5725a01e95d17f71d1d0cb3a6a72f
Kian738 <56565697+Kian738@users.noreply.github.com>
2023-10-29 18:01:53 +0100

    Some type refactoring

--- Source/Boot/EFI/Types.h
+++ Source/Boot/EFI/Types.h
@@ -8,11 +8,7 @@
 #define CONST const
 
 #define VOID void
-typedef UINT8 BOOLEAN;
 typedef UINT8 CHAR8;
 typedef UINT16 CHAR16;
 typedef UINT64 UINTN;
 typedef INT64 INTN;
-
-#define TRUE 1
-#define FALSE 0
--- Source/Common.h
+++ Source/Common.h
@@ -1,5 +1,10 @@
 #pragma once
 
+#define TRUE 1
+#define FALSE 0
+
+typedef UINT8 BOOLEAN;
+
 typedef signed char INT8;
 typedef signed short INT16;
 typedef signed int INT32;
