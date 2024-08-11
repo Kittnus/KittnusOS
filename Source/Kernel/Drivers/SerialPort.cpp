bf55dd281bfb4efa034f76b6d88fec833e47fc82
Kian738 <56565697+Kian738@users.noreply.github.com>
2024-08-11 13:12:48 +0200

    refactor: SerialPort driver should now work appropriately

--- Source/Kernel/Drivers/SerialPort.cpp
+++ Source/Kernel/Drivers/SerialPort.cpp
@@ -2,7 +2,16 @@
 
 #include "HAL/IO.h"
 
-void SerialPort::Initialize() {}
+SerialPort::SerialPort(UInt16 port)
+    : m_Com(port)
+{
+  SetBaudRate(MAX_BAUD_RATE / BAUD_RATE);
+  ConfigureLineControl();
+  ConfigureFifoControl();
+  ConfigureModemControl();
+
+  // Here we configure IRQ4 (COM1 & COM3) or IRQ3 (COM2 & COM4)
+}
 
 void SerialPort::Print(const char* data)
 {
@@ -16,4 +25,36 @@ void SerialPort::PrintLn(const char* data)
   Print("\r\n");
 }
 
-void SerialPort::WriteByte(UInt8 byte) { IO::Write8(COM1, byte); }
+void SerialPort::SetBaudRate(UInt16 divisor)
+{
+  IO::Write8(LINE_CONTROL_PORT(m_Com), 0b10000000);
+  IO::Write8(DATA_PORT(m_Com), (divisor >> 8) & 0xFF);
+  IO::Write8(DATA_PORT(m_Com), divisor & 0xFF);
+}
+
+void SerialPort::ConfigureLineControl()
+{
+  IO::Write8(LINE_CONTROL_PORT(m_Com), 0b00000011);
+}
+
+void SerialPort::ConfigureFifoControl()
+{
+  IO::Write8(FIFO_CONTROL_PORT(m_Com), 0b11000111);
+}
+
+void SerialPort::ConfigureModemControl()
+{
+  IO::Write8(MODEM_CONTROL_PORT(m_Com), 0b00001011);
+}
+
+bool SerialPort::IsTransmitFifoEmpty()
+{
+  return IO::Read8(LINE_STATUS_PORT(m_Com)) & 0b00100000;
+}
+
+void SerialPort::WriteByte(UInt8 byte)
+{
+  while (!IsTransmitFifoEmpty());
+
+  IO::Write8(DATA_PORT(m_Com), byte);
+}
--- Source/Kernel/Drivers/SerialPort.h
+++ Source/Kernel/Drivers/SerialPort.h
@@ -3,27 +3,39 @@
 #include "Types.h"
 
 #define COM1 0x3F8
+#define COM2 0x2F8
+#define COM3 0x3E8
+#define COM4 0x2E8
 
-#define BAUD_RATE    9600
-#define BAUD_DIVISOR 115200 / BAUD_RATE
+#define MAX_BAUD_RATE 115200
+#define BAUD_RATE     9600
 
-#define DATA_PORT          COM1
-#define INTERRUPT_PORT     (COM1 + 1)
-#define FIFO_CONTROL_PORT  (COM1 + 2)
-#define LINE_CONTROL_PORT  (COM1 + 3)
-#define MODEM_CONTROL_PORT (COM1 + 4)
-#define LINE_STATUS_PORT   (COM1 + 5)
-#define MODEM_STATUS_PORT  (COM1 + 6)
-#define SCRATCH_PORT       (COM1 + 7)
+#define DATA_PORT(COM)          COM
+#define INTERRUPT_PORT(COM)     (COM + 1)
+#define FIFO_CONTROL_PORT(COM)  (COM + 2)
+#define LINE_CONTROL_PORT(COM)  (COM + 3)
+#define MODEM_CONTROL_PORT(COM) (COM + 4)
+#define LINE_STATUS_PORT(COM)   (COM + 5)
+#define MODEM_STATUS_PORT(COM)  (COM + 6)
+#define SCRATCH_PORT(COM)       (COM + 7)
 
 class SerialPort
 {
-public:
-  static void Initialize();
+private:
+  UInt16 m_Com;
 
-  static void Print(const char* data);
-  static void PrintLn(const char* data);
+public:
+  SerialPort(UInt16 port = COM1);
+
+  void Print(const char* data);
+  void PrintLn(const char* data);
 
 private:
-  static void WriteByte(UInt8 byte);
+  void SetBaudRate(UInt16 divisor);
+  void ConfigureLineControl();
+  void ConfigureFifoControl();
+  void ConfigureModemControl();
+
+  bool IsTransmitFifoEmpty();
+  void WriteByte(UInt8 byte);
 };
